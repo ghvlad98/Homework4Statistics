0f07d3c49d71b6cb061289c62133f33ce79cafd0
ghvlad98@protonmail.com <ghvlad98@protonmail.com>
2022-10-26 02:26:57 -0700

    Homework4

---
+++ BinomialChart/BinomialChart/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.AbsoluteButton = New System.Windows.Forms.Button()
+        Me.Chart = New System.Windows.Forms.PictureBox()
+        Me.SequenceNumber = New System.Windows.Forms.NumericUpDown()
+        Me.TrialsNumber = New System.Windows.Forms.NumericUpDown()
+        Me.SequenceLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProbabilityLabel = New System.Windows.Forms.Label()
+        Me.ProbabilityNumber = New System.Windows.Forms.NumericUpDown()
+        Me.RelativeButton = New System.Windows.Forms.Button()
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SequenceNumber, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrialsNumber, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProbabilityNumber, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AbsoluteButton
+        '
+        Me.AbsoluteButton.Location = New System.Drawing.Point(1097, 10)
+        Me.AbsoluteButton.Name = "AbsoluteButton"
+        Me.AbsoluteButton.Size = New System.Drawing.Size(132, 27)
+        Me.AbsoluteButton.TabIndex = 0
+        Me.AbsoluteButton.Text = "Absolute Frequency"
+        Me.AbsoluteButton.UseVisualStyleBackColor = True
+        '
+        'Chart
+        '
+        Me.Chart.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Chart.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Chart.Location = New System.Drawing.Point(29, 79)
+        Me.Chart.MaximumSize = New System.Drawing.Size(1200, 600)
+        Me.Chart.Name = "Chart"
+        Me.Chart.Size = New System.Drawing.Size(1200, 600)
+        Me.Chart.TabIndex = 1
+        Me.Chart.TabStop = False
+        '
+        'SequenceNumber
+        '
+        Me.SequenceNumber.Location = New System.Drawing.Point(194, 12)
+        Me.SequenceNumber.Maximum = New Decimal(New Integer() {200, 0, 0, 0})
+        Me.SequenceNumber.Name = "SequenceNumber"
+        Me.SequenceNumber.Size = New System.Drawing.Size(120, 23)
+        Me.SequenceNumber.TabIndex = 2
+        '
+        'TrialsNumber
+        '
+        Me.TrialsNumber.Location = New System.Drawing.Point(518, 12)
+        Me.TrialsNumber.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.TrialsNumber.Name = "TrialsNumber"
+        Me.TrialsNumber.Size = New System.Drawing.Size(120, 23)
+        Me.TrialsNumber.TabIndex = 3
+        '
+        'SequenceLabel
+        '
+        Me.SequenceLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.SequenceLabel.Location = New System.Drawing.Point(12, 11)
+        Me.SequenceLabel.Name = "SequenceLabel"
+        Me.SequenceLabel.Size = New System.Drawing.Size(176, 23)
+        Me.SequenceLabel.TabIndex = 4
+        Me.SequenceLabel.Text = "Number of sequences:"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(369, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 23)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Number of trials:"
+        '
+        'ProbabilityLabel
+        '
+        Me.ProbabilityLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ProbabilityLabel.Location = New System.Drawing.Point(819, 14)
+        Me.ProbabilityLabel.Name = "ProbabilityLabel"
+        Me.ProbabilityLabel.Size = New System.Drawing.Size(98, 23)
+        Me.ProbabilityLabel.TabIndex = 7
+        Me.ProbabilityLabel.Text = "Probability:"
+        '
+        'ProbabilityNumber
+        '
+        Me.ProbabilityNumber.DecimalPlaces = 2
+        Me.ProbabilityNumber.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ProbabilityNumber.Increment = New Decimal(New Integer() {1, 0, 0, 131072})
+        Me.ProbabilityNumber.Location = New System.Drawing.Point(923, 14)
+        Me.ProbabilityNumber.Maximum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.ProbabilityNumber.Name = "ProbabilityNumber"
+        Me.ProbabilityNumber.Size = New System.Drawing.Size(90, 23)
+        Me.ProbabilityNumber.TabIndex = 8
+        '
+        'RelativeButton
+        '
+        Me.RelativeButton.Location = New System.Drawing.Point(1097, 43)
+        Me.RelativeButton.Name = "RelativeButton"
+        Me.RelativeButton.Size = New System.Drawing.Size(132, 23)
+        Me.RelativeButton.TabIndex = 9
+        Me.RelativeButton.Text = "Relative Frequency"
+        Me.RelativeButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.RelativeButton)
+        Me.Controls.Add(Me.ProbabilityNumber)
+        Me.Controls.Add(Me.ProbabilityLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SequenceLabel)
+        Me.Controls.Add(Me.TrialsNumber)
+        Me.Controls.Add(Me.SequenceNumber)
+        Me.Controls.Add(Me.Chart)
+        Me.Controls.Add(Me.AbsoluteButton)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1280, 720)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(1280, 720)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SequenceNumber, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrialsNumber, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProbabilityNumber, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents AbsoluteButton As Button
+    Friend WithEvents Chart As PictureBox
+    Friend WithEvents SequenceNumber As NumericUpDown
+    Friend WithEvents TrialsNumber As NumericUpDown
+    Friend WithEvents SequenceLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProbabilityLabel As Label
+    Friend WithEvents ProbabilityNumber As NumericUpDown
+    Friend WithEvents RelativeButton As Button
+End Class
---
+++ BinomialChart/BinomialChart/Form1.vb
@@ -0,0 +1,142 @@
+﻿Public Class Form1
+
+    Public img As Image
+    Public g As Graphics
+    Public r As New Random
+    Private view As New Rectangle
+
+    Private Sub AbsoluteButton_Click(sender As Object, e As EventArgs) Handles AbsoluteButton.Click
+        Dim widthHisto = 170
+        Dim widthChart = Chart.Width - widthHisto
+        Chart.Image = New Bitmap(Chart.Width, Chart.Height)
+        Me.img = Chart.Image
+        Me.g = Graphics.FromImage(Me.img)
+
+        Dim sequences As Integer = SequenceNumber.Value
+        Dim trials As Integer = TrialsNumber.Value
+        Dim successProb As Double = ProbabilityNumber.Value
+
+        Dim minX As Double = 0
+        Dim maxX As Double = trials
+        Dim minY As Double = 0
+        Dim maxY As Double = trials
+
+        Dim histoBrush As New SolidBrush(Color.Orange)
+        Dim histoPen As New Pen(Color.Gray)
+        Dim recWidth As Double = Chart.Height / sequences
+        Dim histoTop = 0
+        For i As Integer = 1 To sequences
+            Dim pen As New Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)))
+            Dim path As New List(Of Point)
+            Dim y As Double = 0
+            For x As Integer = 1 To trials
+                Dim failProb As Double = r.NextDouble
+                While failProb = successProb
+                    failProb = r.NextDouble
+                End While
+
+                If failProb < successProb Then
+                    y += 1
+                End If
+                Dim newX As Integer = ConvertX(x, minX, maxX, widthChart)
+                Dim newY As Integer = ConvertY(y, minY, maxY, Chart.Height)
+                path.Add(New Point(newX, newY))
+            Next
+            g.DrawLines(pen, path.ToArray)
+
+            Dim recHeight As Integer = ConvertY(y, minY, maxY, widthHisto)
+            g.FillRectangle(histoBrush, New Rectangle(widthChart, histoTop, recHeight, recWidth))
+            g.DrawRectangle(histoPen, New Rectangle(widthChart, histoTop, recHeight, recWidth))
+            histoTop += recWidth
+        Next
+
+    End Sub
+
+    Function ConvertX(x As Double, minX As Double, maxX As Double, w As Integer) As Integer
+        If (maxX - minX) = 0 Then
+            Return 0
+        End If
+
+        Return w * (x - minX) / (maxX - minX)
+
+    End Function
+
+    Function ConvertY(y As Double, minY As Double, maxY As Double, h As Integer) As Integer
+        If (maxY - minY) = 0 Then
+            Return 0
+        End If
+
+        Return h - h * (y - minY) / (maxY - minY)
+
+    End Function
+
+    Private Sub RelativeButton_Click(sender As Object, e As EventArgs) Handles RelativeButton.Click
+        Dim widthHisto = 170
+        Dim widthChart = Chart.Width - widthHisto
+        Chart.Image = New Bitmap(Chart.Width, Chart.Height)
+        Me.img = Chart.Image
+        Me.g = Graphics.FromImage(Me.img)
+
+        Dim sequences As Integer = SequenceNumber.Value
+        Dim trials As Integer = TrialsNumber.Value
+        Dim successProb As Double = ProbabilityNumber.Value
+
+        Dim histoBrush As New SolidBrush(Color.Orange)
+        Dim histoPen As New Pen(Color.Gray)
+        Dim recWidth As Double = Chart.Height / sequences
+        Dim histoTop = 0
+        For i As Integer = 1 To sequences
+            Dim pen As New Pen(Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255)))
+            Dim res As List(Of Integer) = simulate(successProb, trials)
+            Dim path As List(Of Point) = createPath(res, trials, widthChart).Item1
+            Dim mean As Double = createPath(res, trials, widthChart).Item2
+            g.DrawLines(pen, path.ToArray)
+
+            Dim recHeight As Integer = ConvertY(mean, 0, 1, widthHisto)
+            g.FillRectangle(histoBrush, New Rectangle(widthChart, histoTop, recHeight, recWidth))
+            g.DrawRectangle(histoPen, New Rectangle(widthChart, histoTop, recHeight, recWidth))
+            histoTop += recWidth
+        Next
+
+    End Sub
+
+    Function simulate(prob As Double, trials As Integer) As List(Of Integer)
+        Dim results As New List(Of Integer)()
+        For x As Integer = 1 To trials
+            Dim failProb As Double = r.NextDouble()
+            While failProb = prob
+                failProb = r.NextDouble()
+            End While
+            If failProb < prob Then
+                results.Add(1)
+            Else
+                results.Add(0)
+            End If
+        Next
+
+        Return results
+    End Function
+
+    Function createPath(results As List(Of Integer), trials As Integer, widthChart As Integer) As (List(Of Point), Double)
+        Dim minX As Double = 0
+        Dim maxX As Double = trials
+        Dim minY As Double = 0
+        Dim maxY As Double = 1
+
+        Dim path As New List(Of Point)
+        Dim mean As Double = 0
+        For i As Integer = 1 To trials - 1
+            If i = 0 Then
+                mean = results(0)
+            Else
+                mean += (results(i) - mean) / (i + 1)
+            End If
+            Dim newX As Integer = ConvertX(i + 1, minX, maxX, widthChart)
+            Dim newY As Integer = ConvertY(mean, minY, maxY, Chart.Height)
+            path.Add(New Point(newX, newY))
+        Next
+        Console.WriteLine(path)
+        Return (path, mean)
+    End Function
+
+End Class
